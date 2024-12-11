2659a5872efaf7c6500437815ea8e2ee1e4ccd30
Savo Lekovic <savo.lekovic@amplitudo.me>
2024-12-11 14:23:14 +0100

    commit

--- main.py
+++ main.py
@@ -100,8 +100,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS, 
-    allow_credentials=False,
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization", "Accept"],
 )
